a960d332b04116d6f18b2fbcf16a4796b087d53f
Gadjeid <adjeidarkogd@gmail.com>
2025-03-31 01:05:20 -0500

    project shmup

--- Assets/__Scripts/Enemy_0.cs
+++ Assets/__Scripts/Enemy_0.cs
@@ -1,16 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_0 : MonoBehaviour
+public class Enemy_0 : Enemy
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
